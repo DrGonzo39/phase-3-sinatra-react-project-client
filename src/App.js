f24fad8dfadb0b1eb954f6142946c302154e3670
Arthur Benware <artpolitic91@gmail.com>
2023-04-23 16:54:15 -0400

    added initial code to app component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React from "react";
+import { Route, Switch } from "react-router-dom"
+import Home from "./Home";
 import './App.css';
+import BikeList from "./BikeList";
+import UserForm from "./UserForm";
+import NavBar from "./NavBar";
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar/>
+      <Switch>
+        <Route exact path="/">
+        <Home/>
+        </Route>
+        <Route exact path="/bikelist">
+        <BikeList/>
+        </Route> 
+        <Route exact path="/userform">
+        <UserForm/>
+        </Route>
+      </Switch>
     </div>
   );
 }
