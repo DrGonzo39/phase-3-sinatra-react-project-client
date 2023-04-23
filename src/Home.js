abfefc1a2ba3911d00e170b59bb0abea31165ecc
Arthur Benware <artpolitic91@gmail.com>
2023-04-23 17:10:08 -0400

    added Home component

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home() {
+
+    return (
+        <div>
+            <h1>Welcome to Benware's New and Used Bike Store</h1>
+             <img className="home image" src="https://tse1.mm.bing.net/th?id=OIP.WFsxNeeyvF7oyNG11kd0cQHaE6&pid=Api&P=0" alt="Home page image" />
+             <p>This is your #1 online source buy and sell your bikes. All riders welcome!</p>
+        </div>
+    )
+}
+
+export default Home; 
