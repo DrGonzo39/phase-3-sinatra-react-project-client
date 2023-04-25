d448d0ef6e24b8731b22200c0b9ff57e677f5f5c
Arthur Benware <artpolitic91@gmail.com>
2023-04-25 15:33:52 -0400

    added state and post request to BikeForm

--- src/BikeForm.js
+++ src/BikeForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 function BikeForm({ onAddBike }) {
-    const [fromData, setFormData] = useState({
+    const [formData, setFormData] = useState({
         name: "",
         brand: "",
         terrain: "",
@@ -10,6 +10,34 @@ function BikeForm({ onAddBike }) {
         image: "",
         user_id: 0
     })
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        fetch("http://localhost:9292/bikes", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                formData
+            }),
+        })
+        .then((r) => r.json())
+        .then((newBike) => onAddBike(newBike))
+    }
+
+    function handleChange(e) {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value 
+        })
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            
+        </form>
+    )
 }
 
 export default BikeForm
