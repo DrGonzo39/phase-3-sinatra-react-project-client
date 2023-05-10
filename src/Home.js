5ff84f79f382764d903b8217faf3207c67d3eb97
Arthur Benware <artpolitic91@gmail.com>
2023-05-10 19:57:45 -0400

    styled homepage

--- src/Home.js
+++ src/Home.js
@@ -4,9 +4,9 @@ function Home() {
 
     return (
         <div>
-            <h1>Welcome to Benware's New and Used Bike Store</h1>
+            <h1 className="home_header">Welcome to Benware's New and Used Bike Store</h1>
              <img className="home_image" src="https://tse1.mm.bing.net/th?id=OIP.WFsxNeeyvF7oyNG11kd0cQHaE6&pid=Api&P=0" alt="Main site" />
-             <p>This is your #1 online source buy and sell your bikes. All riders welcome!</p>
+             <p className="home_info">This is your #1 online source buy and sell your bikes. All riders welcome!</p>
         </div>
     )
 }
